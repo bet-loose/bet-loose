cea11d2802d6525c01dbb8deb8cc0f5439f74282
gabrieldutrv <gabriel.ld20@aluno.ifsc.edu.br>
2025-05-21 18:23:42 +0000

    Adicionar segunda cena

--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -1,7 +1,21 @@
 import { Scene } from "phaser";
 
 export class Boot extends Scene {
-    constructor() {
-        super({ key: "Boot" })
-    }
+  constructor() {
+    super({ key: "Boot" });
+  }
+
+  preload() {
+    this.load.image("");
+  }
+
+  create() {
+    this.add
+      .image(400, 225, "")
+      .setInteractive()
+      .on("pointerdown", () => {
+        this.scene.stop("");
+        this.scene.start("Preloader");
+      });
+  }
 }
